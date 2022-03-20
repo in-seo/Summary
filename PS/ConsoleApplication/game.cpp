7ec71e1446ae1b416f2046c5703eb3f46abb12cb
in-seo <sis000512@gmail.com>
2022-03-20 17:05:07 +0900

    move game

--- PS/ConsoleApplication/game.cpp
+++ PS/ConsoleApplication/game.cpp
@@ -3,90 +3,70 @@
 #include <algorithm>
 using namespace std;
 
-class Coordinate {
-public:
-	int x;
-	int y;
-	int bl;
-
-	/*Coordinate(int a, int b) {
-		x = a;
-		y = b;
-	};*/
-};
-
-int nextmove(int direction) {
-	direction++;
-	if (direction > 3)
-		direction = 0;
-	return direction;
+int d;
+int turnleft() {
+	d++;
+	if (d > 3)
+		d = 0;
+	return d;
 }
 
 int main() {
-	int n, m,cnt=0;
-	cin >> n >> m;  // n : 가로 , m : 세로
-	int x, y, d;
+	int n, m,move=0;
+	cin >> n >> m;  // n : 세로 , m : 가로
+	int** coor = new int* [n];
+	int x, y;
 	cin >> x >> y >> d;
-	Coordinate** coor = new Coordinate*[m];
+	
 	/*
 	1 1 1 1
 	1 0 0 1
 	1 1 0 1
 	1 1 1 1
 	*/
+	int dx[4] = { -1,0,1,0 }; //세로
+	int dy[4] = { 0,1,0,-1 }; //가로
+	
 	for (int i = 0; i < m; i++)
 	{
-		coor[i] = new Coordinate[n];
-		for (int j = 0; j < n; j++)
+		coor[i] = new int[m];
+		for (int j = 0; j < m; j++)
 		{
-			cin >> coor[i][j].bl;     
+			cin >> coor[i][j];
 		}
 	}
-	
-	coor[x][y].bl = true;
-	cnt++;  //start
-	int direction = nextmove(d);
+	coor[x][y] = 1;
+	move++;
+	int turnCnt = 0;
 	while (true) {
-		bool possible = false;
-		if (direction == 0) {
-			if (coor[x - 1][y].bl == 0) {
-				coor[x - 1][y].bl = 1;
-				x -= 1;
-				cnt++;
-				possible = true;
-			}
-				direction = nextmove(direction);
+		turnleft();
+		turnCnt++;
+		int tempX = x + dx[d];
+		int tempY = y + dy[d];
+		
+		if (coor[tempX][tempY] == 0) {
+			x = tempX;	y = tempY;
+			move++;
+			coor[x][y] = 1;
+			std::cout << "x = " << x << "y = " << y << '\n';
 		}
-		else if (direction == 1) {
-			if (coor[x][y+1].bl == 0) {
-				coor[x][y + 1].bl = 1;
-				y -= 1;
-				cnt++;
-				possible = true;
+		else {
+			if (turnCnt == 4) {
+				x = x - dx[d];
+				y = y - dy[d];
+				if (coor[x][y] == 0) {
+					move++;
+					coor[x][y] = 1;
+					turnCnt = 0;
+				}
+				else 
+					break;
+					
+				}
+					
 			}
-			direction = nextmove(direction);
 		}
-		else if (direction == 2) {
-			if (coor[x+1][y].bl == 0) {
-				coor[x+1][y].bl = 1;
-				x +=1 ;
-				cnt++;
-				possible = true;
-			}
-			direction = nextmove(direction);
-		}
-		else if (direction == 3) {
-			if (coor[x][y-1].bl == 0) {
-				coor[x][y-1].bl = 1;
-				y-=1;
-				cnt++;
-				possible = true;
-			}
-			direction = nextmove(direction);
-		}
-		if(!possible)
-
-	}
+	std::cout << move << endl;
 	
 	
 }
