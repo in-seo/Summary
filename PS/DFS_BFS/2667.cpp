6a692444683632349265d8073340c201e1380084
in-seo <sis000512@gmail.com>
2022-03-24 11:51:57 +0900

    BFS

--- PS/DFS_BFS/2667.cpp
+++ PS/DFS_BFS/2667.cpp
@@ -1,10 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
+
+int n,house;
 int visited[25][25];
+int dx[] = { -1, 0, 1, 0 }; //ºÏ µ¿ ³² ¼­
+int dy[] = { 0, 1, 0, -1 };
 
-void bfs() {
-
+int bfs(int x, int y) {
+	queue<pair<int, int>> q;
+	if (visited[x][y] == 0)
+		return 0;
+	int cnt = 1;
+	visited[x][y] = 0;
+	q.push({ x,y });
+	while (!q.empty()) {
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int mx = x + dx[i];
+			int my = y + dy[i];
+			if (mx >= 0 && my >= 0 && mx < n && my < n) {
+				if (visited[mx][my] == 1) {
+					visited[mx][my] = 0;
+					cnt++;
+					q.push({ mx,my });
+				}
+				if (mx == n && my == n)
+					return cnt;
+			}
+		}
+	}
+	return cnt;
 }
 
 int main() {
@@ -19,6 +47,22 @@ int main() {
 		}
 	}
 
-	cout << bfs(0, 0);
-
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			int n = bfs(i, j);
+			if (n > 0) {
+				house++;
+				v.push_back(n);
+			}
+		}
+	}
+	sort(v.begin(), v.end());
+	cout << house << '\n';
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << '\n';
+	}
 }
