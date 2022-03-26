e7f70f72b62a60576b44a7158afc65c39d92bee0
in-seo <sis000512@gmail.com>
2022-03-26 17:43:10 +0900

    bfs,dfs

--- PS/DFS_BFS/7576.cpp
+++ PS/DFS_BFS/7576.cpp
@@ -5,28 +5,48 @@ int dx[4] = { 0, 1, 0, -1 }; //북 동 남 서
 int dy[4] = { -1, 0, 1, 0 };
 int visited[1000][1000];
 int tom[1000][1000]; 
-// 큐를 반환해서 더한다음 한 텀으로 묶어서 실행시키기
 
-int bfs(int x, int y) {
+queue<pair<int, int>> q;
 
-	queue<pair<int, int>> q;
-	q.push({ x, y });
+void bfs() {
+	int x = q.front().first;
+	int y = q.front().second;
 	visited[y][x] = 1;
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++)
-		{
-			int mx = x + dx[i];
-			int my = y + dy[i];
-			if (mx >= 0 && my >= 0 && mx < m && my < n && visited[my][mx] == 0 && tom[my][mx] == 0) {
-				visited[my][mx] = 1;
-				tom[my][mx] = tom[y][x]+1;
-				q.push({ mx,my });
-			}
+	q.pop();
+	for (int i = 0; i < 4; i++)
+	{
+		int mx = x + dx[i];
+		int my = y + dy[i];
+		if (mx >= 0 && my >= 0 && mx < m && my < n && visited[my][mx] == 0 && tom[my][mx] == 0) {
+			visited[my][mx] = 1;
+			tom[my][mx] = tom[y][x] + 1;
+			q.push({ mx,my });
 		}
 	}
+}
+
+int main() {
+	cin >> m >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int k;
+			cin >> k;
+			if (k == 1) {
+				q.push({ j, i });
+			}
+			tom[i][j] = k;
+		}
+	}
+	if (q.size() == 0) {
+		cout << "-1";
+		return 0;
+	}
+	else if (q.size() == m*n) {
+		cout << "0";
+		return 0;
+	}
+	while (!q.empty())
+		bfs();
 	int max = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
@@ -34,48 +54,16 @@ int bfs(int x, int y) {
 				max = tom[i][j];
 		}
 	}
-	return max - 1;
-}
 
-int main() {
-	cin >> m >> n;
-	int max = 0;
-	bool start = false;
+	bool pass = true;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			int n;
-			cin >> n;
-			if (n == 0)
-				start = true;  //첫 시작 시 모두 1 방지
-			tom[i][j] = n;
+			if (tom[i][j] == 0)
+				pass = false;
 		}
 	}
-
-	if (!start) {
-		cout << "0";
-		return 0;
-	}
-		
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (visited[i][j] == 0 && tom[i][j]==1) {
-				bfs(j, i);
-			}
-		}
-	}
-		
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (tom[i][j] == 0) {
-				cout << -1;
-				return 0;
-			}
-			cout << tom[i][j] << " ";
-		}
-		cout << '\n';
-	}
-
-	
-
-			
+	if (pass)
+		cout << max - 1;
+	else
+		cout << "-1";
 }
