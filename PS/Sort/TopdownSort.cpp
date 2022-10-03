0542b99f553bbda6fd48436362162b19f8e772bb
in-seo <sis000512@gmail.com>
2022-10-03 15:03:36 +0900

    PS

--- PS/Sort/TopdownSort.cpp
+++ PS/Sort/TopdownSort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //pair<string, int> p[100000];
-vector< pair<string, int>> v;
+vector< pair<string, int>> vsort;
 bool comp(pair<string, int> p1, pair<string, int> p2) {
 	return p1.second < p2.second;
 }
@@ -14,15 +14,15 @@ int main() {
 		string str;
 		int n;
 		cin >> str >> n;
-		v.push_back({ str,n });
+		vsort.push_back({ str,n });
 		//cin >> p[i].first >> p[i].second;
 	}
 	//sort(p, p + n, comp);
-	sort(v.begin(), v.end(), comp);
+	sort(vsort.begin(), vsort.end(), comp);
 	for (int i = 0; i < n; i++)
 	{
 		//cout << p[i].first << '\n';
-		cout << v[i].first << '\n';
+		cout << vsort[i].first << '\n';
 	}
 	
 }
